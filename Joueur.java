cb019632336d9f8d0e1f4bb3a79f8b3e588f9c5f
Cirediallo <theevilmonster93@gmail.com>
2018-02-18 15:36:40 +0100

    Create Joueur.java

---
+++ Joueur.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public abstract class Joueur {
+	private String name;
+	
+	protected Paquet paquet;
+	
+	private static int score; //Score du joueur
+	
+	public Joueur(String nom) {
+		this.name = nom;
+		this.score = 0;
+		nouveauJeu();
+	}
+	
+	public void setName(String nom) {
+		this.name = nom;
+	}
+	
+	public void nouveauJeu() {
+		this.paquet = new Paquet();
+	}
+	
+	public Carte jouer() {
+		Carte aJouer = choisirCarte();
+		this.paquet.retirerCarte(aJouer);
+		return aJouer;
+	}
+	
+	public abstract Carte choisirCarte();
+	
+	public void recupererCarte(Carte c) {
+		c.toString();
+	}
+	
+	public boolean aPerdu() {
+		if(this.paquet.estVide()) {
+			return true;
+		}else
+			return false;
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+	
+	public void aGagner() {
+		this.score++;
+	}
+	
+	public int getNombreCartesJoueur() {
+		return this.paquet.getNombreCarte();
+	}
+	
+	public void gagnerCarte(Carte c) {
+		this.paquet.ajouterCarte(c);
+	}
+	
+	public String toString() {
+		return this.name;
+	}
+}
